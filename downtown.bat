fa3984a7e1e1441ae24b9267a9b03e5d2c760d2b
Ian <itfairhope@aol.com>
2018-04-24 23:00:50 -0400

    Update on simulations.

--- downtown.bat
+++ downtown.bat
@@ -1,5 +1,5 @@
-CALL one.bat -b 3 scenarios\downtown\initial\settings\node_50.txt
-CALL one.bat -b 3 scenarios\downtown\initial\settings\node_150.txt
-CALL one.bat -b 3 scenarios\downtown\initial\settings\node_250.txt
-CALL one.bat -b 3 scenarios\downtown\initial\settings\node_350.txt
-CALL one.bat -b 3 scenarios\downtown\initial\settings\node_450.txt
+CALL one.bat -b 5 scenarios\downtown\initial\settings\node_50.txt
+CALL one.bat -b 5 scenarios\downtown\initial\settings\node_150.txt
+CALL one.bat -b 5 scenarios\downtown\initial\settings\node_250.txt
+CALL one.bat -b 5 scenarios\downtown\initial\settings\node_350.txt
+CALL one.bat -b 5 scenarios\downtown\initial\settings\node_450.txt
