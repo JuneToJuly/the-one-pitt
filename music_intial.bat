4758f7a9aed62333c44dbdcccaafc287ee3537d6
Ian <itfairhope@aol.com>
2018-04-25 04:41:01 -0400

    Update on simulations data.

---
+++ music_intial.bat
@@ -0,0 +1,5 @@
+CALL one.bat -b 5 scenarios\music-fest\initial\settings\node_50.txt
+CALL one.bat -b 5 scenarios\music-fest\initial\settings\node_150.txt
+CALL one.bat -b 5 scenarios\music-fest\initial\settings\node_250.txt
+CALL one.bat -b 5 scenarios\music-fest\initial\settings\node_350.txt
+CALL one.bat -b 5 scenarios\music-fest\initial\settings\node_450.txt
---
+++ music_response.bat
@@ -0,0 +1,5 @@
+CALL one.bat -b 5 scenarios\music-fest\response\settings\node_50.txt
+CALL one.bat -b 5 scenarios\music-fest\response\settings\node_150.txt
+CALL one.bat -b 5 scenarios\music-fest\response\settings\node_250.txt
+CALL one.bat -b 5 scenarios\music-fest\response\settings\node_350.txt
+CALL one.bat -b 5 scenarios\music-fest\response\settings\node_450.txt
